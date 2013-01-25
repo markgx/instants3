216fc87660e43351b3637d10209acd91d6b8dd63
Mark Guerra <markgx@gmail.com>
2013-01-24 18:31:45 -0800

    moved photo logic into its own view

---
+++ app/scripts/instantsview.js
@@ -0,0 +1,100 @@
+var InstantsView = Backbone.View.extend({
+  initialize: function() {
+    this.loggedIn = false;
+
+    if (localStorage.accessToken) {
+      this.instagram = new Instagram(this.options.clientID, this.options.callbackURL, '', localStorage.accessToken);
+      this.loggedIn = true;
+    } else {
+      this.instagram = new Instagram(this.options.clientID, this.options.callbackURL, '');
+    }
+
+    this.imageList = new ImageList();
+  },
+
+  events: {
+    'click .auth': 'authorize',
+    'click #about-trigger a': 'toggleAbout',
+    'click #close a': 'hideAbout'
+  },
+
+  render: function() {
+    if (this.loggedIn) {
+      var self = this;
+      var loadFunction = null;
+
+      if (this.options.feedType === FEED_TYPES.FEED) {
+        loadFunction = this._loadFeedImages;
+      } else {
+        loadFunction = this._loadPopularImages;
+      }
+
+      loadFunction.call(self).success(function() {
+        self.interval = setInterval(function() {
+          self._showImage();
+        }, options.intervalMS);
+
+        // call twice -- better way to do this?
+        setTimeout(function() { loadFunction.call(self); }, 20000);
+      });
+    } else {
+      $('#welcome').show();
+    }
+  },
+
+  authorize: function(e) {
+    e.preventDefault();
+    this.instagram.authorize();
+  },
+
+  toggleAbout: function(e) {
+    e.preventDefault();
+    $('#about').toggle();
+  },
+
+  hideAbout: function(e) {
+    e.preventDefault();
+    $('#about').hide();
+  },
+
+  _loadFeedImages: function() {
+    var self = this;
+
+    return this.instagram.getUserFeed(function(result) {
+      self.nextMaxId = result.pagination.next_max_id; // if fetching more images, start from this id
+      self._processFeedResults(result);
+    }, 30, null, self.nextMaxId);
+  },
+
+  _loadPopularImages: function() {
+    var self = this;
+
+    return this.instagram.getPopularFeed(function(result) {
+      self._processFeedResults(result);
+    });
+  },
+
+  _processFeedResults: function(result) {
+    var self = this;
+
+    var images = _.map(result.data, function(o) {
+      return {
+        id: o.id,
+        url: o.images.low_resolution.url,
+        link: o.link,
+        username: o.user.username
+      };
+    });
+
+    // preload images
+    _(images).each(function(el) {
+      self.imageList.add(el);
+      $('<img />').attr('src', el.url).appendTo('#preload').css('display','none');
+    });
+  },
+
+  _showImage: function() {
+    var photoView = new PhotoView({ parentView: this });
+    photoView.render();
+  }
+});
---
+++ app/scripts/photoview.js
@@ -0,0 +1,72 @@
+var PhotoView = Backbone.View.extend({
+  tagName: 'div',
+  className: 'image',
+
+  initialize: function() {
+    this.parentView = this.options.parentView;
+
+    // copy a few options from parent view
+    _(this.options).extend(_(this.parentView.options)
+      .pick('imageLength', 'fadeDelayMS', 'fadeOutMS'));
+  },
+
+  render: function() {
+    var image = this.parentView.imageList.getNextImage();
+    this.$el.append('<img src="' + image.url + '" />')
+
+    // randomize side to throw from
+
+    var side = Math.floor(Math.random() * 4);
+
+    switch (side) {
+      case 0: // top
+        this.$el.css({ top: this.options.imageLength * -1, left: this._randomX() });
+        break;
+      case 1: // right
+        this.$el.css({ top: this._randomY(), left: window.innerWidth });
+        break;
+      case 2: // bottom
+        this.$el.css({ top: window.innerHeight, left: this._randomX() });
+        break;
+      case 3: // left
+        this.$el.css({ top: this._randomY(), left: this.options.imageLength * -1 });
+        break;
+    }
+
+    $('#board').append(this.$el);
+
+    // randomize throw
+
+    var spin = 1;
+    if (Math.round(Math.random()) == 0) {
+      spin = -1;
+    }
+
+    var rotateOffset = Math.floor(Math.random() * 15);
+    var spinCount = Math.floor(Math.random() * 2); // 0 or 1
+
+    var rotate = ((360 * spinCount) + rotateOffset) * spin;
+
+    if (spinCount == 0) {
+      // if no spin, do half a spin
+      this.$el.css({ rotate: 180 });
+    }
+
+    var left = this._randomX();
+    var top = this._randomY();
+
+    this.$el
+      .transition({ left: left, top: top, rotate: rotate, easing: 'snap', duration: 1500 })
+      .transition({ opacity: 0, delay: this.options.fadeDelayMS }, this.options.fadeOutMS, 'in', function() {
+        this.remove();
+      });
+  },
+
+  _randomX: function() {
+    return Math.floor(Math.random() * (window.innerWidth - options.imageLength));
+  },
+
+  _randomY: function() {
+    return Math.floor(Math.random() * (window.innerHeight - options.imageLength));
+  }
+});
