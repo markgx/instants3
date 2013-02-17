34c5a01fe5d1e8c71035d316343d6a141054a62d
Mark Guerra <markgx@gmail.com>
2013-02-16 23:24:12 -0800

    disable jshint for now

--- Gruntfile.js
+++ Gruntfile.js
@@ -253,7 +253,7 @@ module.exports = function (grunt) {
 
     grunt.registerTask('build', [
         'clean:dist',
-        'jshint',
+        // 'jshint',
         'test',
         'coffee',
         'compass:dist',
